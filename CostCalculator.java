07a19880b08945c306101eea713a67b9d53637f2
Amaan Nizam <amaannizam292004@gmail.com>
2026-06-24 03:43:08 +0200

    Update CostCalculator.java

--- CostCalculator.java
+++ CostCalculator.java
@@ -1,9 +1,6 @@
-// Helper class that calculates the rental price.
-// It uses a static method, so no object is needed to call it.
+
 public class CostCalculator {
 
-    // Price = daily rate * number of days.
-    // Rentals of 7 days or more get a 10% discount.
     public static double calculateCost(double dailyRate, int days) {
         double cost = dailyRate * days;
 
